69636eda194a5fe137a24608afed7b97ca56de5a
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-02-19 01:41:31 -0700

    create pydantic setting for env file

---
+++ src/helper/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Setting(BaseSettings):
+
+    HUGGINGFACE_TOKEN: str
+    WANDB_API_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+def get_settings() -> Setting:
+    return Setting()
