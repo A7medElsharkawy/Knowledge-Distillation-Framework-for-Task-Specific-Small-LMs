6be2fd1a7931b4e1cc4896a0030a5a0699d49ad7
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-02-20 05:01:28 -0700

    update file

--- src/evaluation/eval_base_local.py
+++ src/evaluation/eval_base_local.py
@@ -1,17 +1,12 @@
-from urllib3 import response
-from utils.prompt_template import create_details_extraction_prompt
-from models.shcemes import NewsDetails
-from models.enums import  ModelEnum
 from controllers import ModelController
 
-def eval_base_model():
+
+def eval_base_model(model_name,messages):
+
     modelcontroller = ModelController()
-    model, tokenizer = modelcontroller.load_model_and_tokenizer(ModelEnum.BASE_MODEL_QWEN.value)
-
-    messages = create_details_extraction_prompt(NewsDetails)
-
-    text = modelcontroller.apply_chat_templete(messages = messages,tokenizer = tokenizer)
-    response = modelcontroller.model_output(text,tokenizer,model)
+    model, tokenizer = modelcontroller.load_model_and_tokenizer(model_name)
+    output = modelcontroller.apply_chat_templete(messages = messages,tokenizer = tokenizer)
+    response = modelcontroller.model_output(output,tokenizer,model)
 
     return response
 
