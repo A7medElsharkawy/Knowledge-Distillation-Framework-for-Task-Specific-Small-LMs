87bcab4c5f71d30d9f383d502780c0c73517255f
A7medElsharkawy <sharkawynoo271@gmail.com>
2026-02-24 22:36:30 -0700

    add loa_raw_data funcation

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,7 @@
 from .BaseController import BaseController
 import os
-
+import json
+import random
 
 class DataController(BaseController):
     def __init__(self):
@@ -11,4 +12,17 @@ class DataController(BaseController):
         example_path = os.path.join(self.raw_data_dir , "example.txt")
         with open(example_path, "r", encoding="utf-8") as f:
             return f.read().strip()
+    
+    def load_raw_data(self) -> list[str]:
+        raw_data_path = os.path.join(self.raw_data_dir, "news-sample.jsonl")
 
+        raw_data = []
+        
+        for line in open(raw_data_path, "r", encoding="utf-8"):
+            if line.strip() == "":
+                continue
+            raw_data.append(json.loads(line.strip()))
+
+        random.Random(42).shuffle(raw_data)
+        return raw_data
+        
